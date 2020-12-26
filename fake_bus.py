627994a27766507ae046f3aade0fc3fd905792a3
AndreyAD <arakelyants.andrey@yandex.ru>
2020-12-26 15:57:30 +0300

    Add several buses to every route

--- fake_bus.py
+++ fake_bus.py
@@ -1,13 +1,16 @@
 from itertools import cycle
 import json
 import os
+import random
 from sys import stderr
 
 import trio
 from trio_websocket import open_websocket_url, ConnectionClosed
 
+BUS_NUMBER_PER_ROUTE = 3
 
-async def run_bus(url, bus_id, route):
+
+async def run_bus(url, bus_id, route, route_name):
     try:
         async with open_websocket_url(url) as ws:
             for latitude, longitude in cycle(route):
@@ -16,7 +19,7 @@ async def run_bus(url, bus_id, route):
                         'busId': bus_id,
                         'lat': latitude,
                         'lng': longitude,
-                        'route': bus_id
+                        'route': route_name
                     }
                     await ws.send_message(
                         json.dumps(
@@ -39,17 +42,27 @@ def load_routes(directory_path='routes'):
                 yield json.load(file)
 
 
+def generate_bus_id(route_id, bus_index):
+    return f"{route_id}-{bus_index}"
+
+
 async def main():
     async with trio.open_nursery() as nursery:
         for route in load_routes():
-            route_name = route['name']
-            bus_coordinates = route['coordinates']
-            nursery.start_soon(
-                run_bus,
-                'ws://127.0.0.1:8080',
-                route_name,
-                bus_coordinates
-            )
+            for bus_index in range(BUS_NUMBER_PER_ROUTE):
+                bus_id = generate_bus_id(route['name'], bus_index)
+                random_index = random.randrange(0, len(route['coordinates']))
+                bus_coordinates = [
+                    *route['coordinates'][random_index:],
+                    *route['coordinates'][:random_index]
+                ]
+                nursery.start_soon(
+                    run_bus,
+                    'ws://127.0.0.1:8080',
+                    bus_id,
+                    bus_coordinates,
+                    route['name']
+                )
 
 
 if __name__ == '__main__':
