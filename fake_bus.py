e6f4b9b7151228d4692514f571fa1fd0a250298a
AndreyAD <arakelyants.andrey@yandex.ru>
2020-12-28 11:24:46 +0300

    Try to use trio channels

--- fake_bus.py
+++ fake_bus.py
@@ -8,12 +8,30 @@ import trio
 from trio_websocket import open_websocket_url, ConnectionClosed
 
 BUS_NUMBER_PER_ROUTE = 3
+SERVER_URL = 'ws://127.0.0.1:8080'
 
 
-async def run_bus(url, bus_id, route, route_name):
+def load_routes(directory_path='routes'):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r', encoding='utf8') as file:
+                yield json.load(file)
+
+
+def generate_bus_id(route_id, bus_index):
+    return f'{route_id}-{bus_index}'
+
+
+async def run_bus(send_channel, bus_id, bus_coordinates, route_name):
+    for latitude, longitude in cycle(bus_coordinates):
+        await send_channel.send((bus_id, latitude, longitude, route_name))
+
+
+async def send_updates(url, receive_channel):
     try:
         async with open_websocket_url(url) as ws:
-            for latitude, longitude in cycle(route):
+            async for bus_id, latitude, longitude, route_name in receive_channel:
                 try:
                     response = {
                         'busId': bus_id,
@@ -34,20 +52,9 @@ async def run_bus(url, bus_id, route, route_name):
         print('Connection attempt failed: %s' % ose, file=stderr)
 
 
-def load_routes(directory_path='routes'):
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".json"):
-            filepath = os.path.join(directory_path, filename)
-            with open(filepath, 'r', encoding='utf8') as file:
-                yield json.load(file)
-
-
-def generate_bus_id(route_id, bus_index):
-    return f"{route_id}-{bus_index}"
-
-
 async def main():
     async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(0)
         for route in load_routes():
             for bus_index in range(BUS_NUMBER_PER_ROUTE):
                 bus_id = generate_bus_id(route['name'], bus_index)
@@ -58,11 +65,12 @@ async def main():
                 ]
                 nursery.start_soon(
                     run_bus,
-                    'ws://127.0.0.1:8080',
+                    send_channel,
                     bus_id,
                     bus_coordinates,
                     route['name']
                 )
+        nursery.start_soon(send_updates, SERVER_URL, receive_channel)
 
 
 if __name__ == '__main__':
