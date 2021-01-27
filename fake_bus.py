4b6f369c3b22a5cdb78e0f90324070ca3e31c494
Andrey Arakelyants <andrey.arakelyants@myoffice.team>
2021-01-27 18:11:20 +0300

    Make a console script.

--- fake_bus.py
+++ fake_bus.py
@@ -1,18 +1,16 @@
-from itertools import cycle
+from itertools import cycle, islice
 import json
 import os
 import random
 from sys import stderr
 
+import click
 import trio
 from trio_websocket import open_websocket_url, ConnectionClosed
 
-BUS_NUMBER_PER_ROUTE = 35
-SERVER_URL = 'ws://127.0.0.1:8080'
 
-
-def load_routes(directory_path='routes'):
-    for filename in os.listdir(directory_path):
+def load_routes(route_number, directory_path='routes'):
+    for filename in islice(os.listdir(directory_path), route_number):
         if filename.endswith('.json'):
             filepath = os.path.join(directory_path, filename)
             with open(filepath, 'r', encoding='utf8') as file:
@@ -51,11 +49,11 @@ async def send_updates(url, receive_channel):
         print('Connection attempt failed: %s' % ose, file=stderr)
 
 
-async def main():
+async def run_buses(server_url, route_number, buses_per_route):
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
-        for route in load_routes():
-            for bus_index in range(BUS_NUMBER_PER_ROUTE):
+        for route in load_routes(route_number):
+            for bus_index in range(buses_per_route):
                 bus_id = generate_bus_id(route['name'], bus_index)
                 random_index = random.randrange(0, len(route['coordinates']))
                 bus_coordinates = [
@@ -69,8 +67,25 @@ async def main():
                     bus_coordinates,
                     route['name']
                 )
-        nursery.start_soon(send_updates, SERVER_URL, receive_channel)
+        nursery.start_soon(send_updates, server_url, receive_channel)
+
+
+@click.command()
+@click.argument('server_url')
+@click.option(
+    '--route_number',
+    type=click.INT,
+    help='The number of bus routes.'
+)
+@click.option(
+    '--buses_per_route',
+    type=click.INT,
+    help='The number of buses per route.'
+)
+def main(server_url, route_number, buses_per_route):
+    """Send bus coordinates to a server."""
+    trio.run(run_buses, server_url, route_number, buses_per_route)
 
 
 if __name__ == '__main__':
-    trio.run(main)
+    main()
