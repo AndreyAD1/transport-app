4de84e24e4d95f77a35af58b08ce816623742cec
Andrey Arakelyants <andrey.arakelyants@myoffice.team>
2021-01-27 18:47:11 +0300

    Add options

--- fake_bus.py
+++ fake_bus.py
@@ -21,9 +21,10 @@ def generate_bus_id(route_id, bus_index):
     return f'{route_id}-{bus_index}'
 
 
-async def run_bus(send_channel, bus_id, bus_coordinates, route_name):
+async def run_bus(send_channel, bus_id, bus_coordinates, route_name, timeout):
     for latitude, longitude in cycle(bus_coordinates):
         await send_channel.send((bus_id, latitude, longitude, route_name))
+        await trio.sleep(timeout)
 
 
 async def send_updates(url, receive_channel):
@@ -49,12 +50,13 @@ async def send_updates(url, receive_channel):
         print('Connection attempt failed: %s' % ose, file=stderr)
 
 
-async def run_buses(server_url, route_number, buses_per_route, ws_number):
+async def run_buses(server_url, route_number, buses_per_route, ws_number, emulator_id, refresh_timeout):
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
         for route in load_routes(route_number):
             for bus_index in range(buses_per_route):
                 bus_id = generate_bus_id(route['name'], bus_index)
+                bus_id = emulator_id + bus_id
                 random_index = random.randrange(0, len(route['coordinates']))
                 bus_coordinates = [
                     *route['coordinates'][random_index:],
@@ -65,7 +67,8 @@ async def run_buses(server_url, route_number, buses_per_route, ws_number):
                     send_channel,
                     bus_id,
                     bus_coordinates,
-                    route['name']
+                    route['name'],
+                    refresh_timeout
                 )
         for _ in range(ws_number):
             nursery.start_soon(send_updates, server_url, receive_channel)
@@ -91,14 +94,27 @@ async def run_buses(server_url, route_number, buses_per_route, ws_number):
     type=click.IntRange(1),
     help='The number of open websockets.'
 )
-def main(server_url, route_number, buses_per_route, websocket_number):
+@click.option(
+    '--emulator_id',
+    default='',
+    help='This string will be the prefix of every bus id.'
+)
+@click.option(
+    '--refresh_timeout',
+    default=1,
+    type=click.IntRange(1),
+    help='The timeout to update bus coordinates (seconds).'
+)
+def main(server_url, route_number, buses_per_route, websocket_number, emulator_id, refresh_timeout):
     """Send bus coordinates to a server."""
     trio.run(
         run_buses,
         server_url,
         route_number,
         buses_per_route,
-        websocket_number
+        websocket_number,
+        emulator_id,
+        refresh_timeout
     )
 
 
