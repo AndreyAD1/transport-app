0267eddfa579feed9f1334cef0c351d415b1e9c5
AndreyAD <arakelyants.andrey@yandex.ru>
2021-02-27 01:09:28 +0300

    Improve the tests

--- tests/test_browser_client.py
+++ tests/test_browser_client.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 import pytest
 from trio_websocket import open_websocket_url
@@ -18,6 +19,74 @@ async def test_invalid_browser_json(invalid_message):
         response = await ws.get_message()
         expected_response = {
             'errors': ['Requires valid JSON'],
+            'msgType': 'Errors'
+        }
+        try:
+            json_response = json.loads(response)
+        except json.JSONDecodeError:
+            assert False, f'Can not unmarshal response to JSON: {response}'
+
+        assert json_response == expected_response, 'Invalid JSON'
+
+
+async def test_empty_dict_json():
+    async with open_websocket_url('ws://127.0.0.1:8000') as ws:
+        await ws.get_message()
+        await ws.send_message('{}')
+        response = await ws.get_message()
+        expected_response = {
+            'errors': [f'Requires msgType specified'],
+            'msgType': 'Errors'
+        }
+        try:
+            json_response = json.loads(response)
+        except json.JSONDecodeError:
+            assert False, f'Can not unmarshal response to JSON: {response}'
+
+        assert json_response == expected_response, 'Invalid JSON'
+
+
+async def test_empty_list_json():
+    async with open_websocket_url('ws://127.0.0.1:8000') as ws:
+        await ws.get_message()
+        await ws.send_message('[]')
+        response = await ws.get_message()
+        expected_response = {
+            "errors": ["Requires a mapping JSON root element"],
+            'msgType': 'Errors'
+        }
+        try:
+            json_response = json.loads(response)
+        except json.JSONDecodeError:
+            assert False, f'Can not unmarshal response to JSON: {response}'
+
+        assert json_response == expected_response, 'Invalid JSON'
+
+
+@pytest.mark.parametrize(
+    ['message', 'absent_field_name'],
+    [
+        ({'msgType': 'errors'}, 'data'),
+        (
+            {
+                'data': {
+                    'east_lng': random.uniform(0, 90),
+                    'north_lat': random.uniform(0, 90),
+                    'south_lat': random.uniform(0, 90),
+                    'west_lng': random.uniform(0, 90)
+                }
+            },
+            'msgType'
+        ),
+    ]
+)
+async def test_absent_fields(message, absent_field_name):
+    async with open_websocket_url('ws://127.0.0.1:8000') as ws:
+        await ws.get_message()
+        await ws.send_message(json.dumps(message))
+        response = await ws.get_message()
+        expected_response = {
+            'errors': [f'Requires {absent_field_name} specified'],
             'msgType': 'Errors'
         }
         try:
